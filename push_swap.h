0c1634eed86096b261f49f9f22e9db0b62b39ec1
Camille BARBIT <cbarbit@e2r2p4.clusters.42paris.fr>
2022-02-28 16:27:34 +0100

    START

---
+++ push_swap.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/28 15:50:58 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/02/28 16:08:46 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+#include "ft_printf.h"
+#include "libft.h"
+
+
+typdef struct   s_stacks;
+{
+	int *stack_a;
+	int	*stack_b;
+	int	*temp;
+	int	size_a;
+	int	size_b;
+
+} t_stacks;
+
+
+
+
+
+
+#endif
