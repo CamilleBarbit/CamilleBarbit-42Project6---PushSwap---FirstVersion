c3d3465541f62247b61344b2e84cd138dde495ba
Camille BARBIT <cbarbit@e2r2p4.clusters.42paris.fr>
2022-02-28 18:37:36 +0100

    Updated version

--- Instructions/push.c
+++ Instructions/push.c
@@ -6,38 +6,58 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 17:01:31 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/28 17:34:14 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/28 18:35:39 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void    pa(t_stacks push_swap)
-{
-    int tempo;
-    
-    if (!t_stacks->stack_b)
-        return ;
-    tempo = t_stacks->stack_b[0];
-    int i = 0;
-    int temp = t_stacks->stack_a[0];
-    int temp2;
-    while (i < t_stacks->size_a)
-    {
-        temp2 = temp;
-        temp = t_stacks->stack_a[i + 1];
-        t_stacks->stack_a[++i] = temp2;
-    }
-    t_stacks->size_a[0] = tempo;
-    t_stacks->size_a++;
 
-    i = t_stacks->size_b - 1;
-    temp = t_stacks->stack_b[i];
-    while (i > 0)
-    {
-        temp2 = temp;
-        temp = t_stacks->stack_b[i - 1];
-        t_stacks->stack_b[--i] = temp2;
-    }
-    t_stacks->size_b--;
+void	add_top(int *tab, int nb, int size)
+{
+	int tempo1;
+	int tempo2;
+	int i;
+    
+	i = 0;
+	tempo1 = tab[0];
+	while (i < size)
+	{
+		tempo2 = tempo1;
+		tempo1 = tab[i + 1];
+		tab[i + 1] = tempo2;
+		i++;
+	}
+	tab[0] = nb;
+	size += 1;  
+}
+
+void	remove_top(int *tab, int size)
+{
+	int tempo1;
+	int tempo2;
+	int i;
+
+	i = size - 1;
+	tempo1 = tab[i];
+	while (i > 0)
+	{
+		tempo2 = tempo1;
+		tempo1 = tab[i - 1];
+		tab[i - 1] = tempo2;
+		i--;
+	}
+	size -= 1;
+}
+
+void	pa(t_stacks *stack_a, t_stacks *stack_b)
+{
+	add_top(stack_a->tab_a, stack_b->tab_b[0], stack_a->size_a);
+	remove_top(stack_b->tab_b, stack_b->size_b);
+}
+
+void	pb(t_stacks *stack_a, t_stacks *stack_b)
+{
+	add_top(stack_b->tab_b, stack_a->tab_a[0], stack_b->size_b);
+	remove_top(stack_a->tab_a, stack_a->size_a);
 }
