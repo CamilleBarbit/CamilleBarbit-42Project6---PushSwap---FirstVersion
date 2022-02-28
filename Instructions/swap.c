2f7ce2bd0c070dcc8c3a5e183118b0dfd8ecf091
Camille BARBIT <cbarbit@e2r2p4.clusters.42paris.fr>
2022-02-28 17:53:59 +0100

    Updated

--- Instructions/swap.c
+++ Instructions/swap.c
@@ -6,36 +6,36 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:20:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/28 17:10:34 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/28 17:53:35 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void	sa(t_stacks push_swap)
+void	sa(t_stacks stack_a)
 {
 	int tempo;
 
-	if (push_swap->size_a <= 1)
+	if (stack_a->size_a <= 1)
 		return ;
-	temp = push_swap->stack_a[0];
-	push_swap->stack_a[0] = push_swap->stack_a[1];
-	push_swap->stack_a[1] = tempo;
+	temp = stack_a->tab_a[0];
+	stack_a->tab_a[0] = stack_a->tab_a[1];
+	stack_a->tab_a[1] = tempo;
 }
 
-void	sb(t_stacks push_swap)
+void	sb(t_stacks stack_b)
 {
 	int tempo;
 
-	if (push_swap->size_a <= 1)
+	if (stack_b->size_a <= 1)
 		return ;
-	temp = push_swap->stack_b[0];
-	push_swap->stack_b[0] = push_swap->stack_a[1];
-	push_swap->stack_b[1] = tempo;
+	temp = stack_b->tab_b[0];
+	stack_b->tab_b[0] = stack_b->tab_b[1];
+	stack_a->tab_b[1] = tempo;
 }
 
-void	ss(t_stacks push_swap)
+void	ss(t_stacks stack_a, t_stacks stack_b)
 {
-	sa(push_swap);
-	sb(push_swap);
+	sa(stack_a);
+	sb(stack_b);
 }
