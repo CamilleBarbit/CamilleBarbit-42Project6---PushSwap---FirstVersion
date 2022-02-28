56bf11307213e21d05e13c9827694e4c6093de80
Camille BARBIT <cbarbit@e2r2p4.clusters.42paris.fr>
2022-02-28 16:33:29 +0100

    START updated

--- Instructions/swap.c
+++ Instructions/swap.c
@@ -6,16 +6,20 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:20:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/28 16:25:28 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/02/28 16:33:14 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-void    sa(t_stacks)
+void	sa(t_stacks)
 {
-    if (push_swap->size_a == 0)
-        return (1);
-    
-    
+	int temp;
+
+	temp = push_swap->stack_a[0];
+	if (push_swap->size_a <= 1)
+		return ;
+	push_swap->stack_a[0] = push_swap->stack_a[1];
+	push_swap->stack_a[1] = temp;
 }
+
