ebeff45e1530a5ea628b43cae717d19326fec3c5
Camille BARBIT <cbarbit@e3r4p4.clusters.42paris.fr>
2022-03-01 10:30:21 +0100

    Corrected mistakes with structures

--- push_swap.h
+++ push_swap.h
@@ -6,30 +6,39 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 15:50:58 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/02/28 17:52:19 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/01 10:27:11 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "ft_printf.h"
-#include "libft.h"
+/*
+LIBRARIES
+*/
 
+#include "./ft_printf/ft_printf.h"
+#include "./libft/libft.h"
 
-typdef struct   s_a;
+/*
+STRUCTURES
+*/
+
+typedef struct s_a	t_a;
+struct s_a
 {
 	int *tab_a;
 	int	size_a;
 
-} t_a;
+};
 
-typdef struct   s_b;
+typedef struct s_b	t_b;
+struct s_b
 {
 	int *tab_b;
 	int	size_b;
 
-} t_b;
+};
 
 
 
