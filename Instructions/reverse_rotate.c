b27060f61720797e783e2daa521bd61a3df15c66
Camille BARBIT <cbarbit@e3r4p4.clusters.42paris.fr>
2022-03-01 12:35:06 +0100

    All working fine

--- Instructions/reverse_rotate.c
+++ Instructions/reverse_rotate.c
@@ -6,9 +6,44 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 18:51:23 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/01 11:17:34 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/01 12:34:37 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
+void	rra(t_a *stack_a)
+{
+	int tempo1;
+	int tempo2;
+	int i;
+
+	i = 0;
+	tempo1 = stack_a->tab[0];
+	while (i < stack_a->size)
+	{
+		tempo2 = tempo1;
+		tempo1 = stack_a->tab[i + 1];
+		stack_a->tab[i + 1] = tempo2;
+		i++;
+	} 
+	stack_a->tab[0] = tempo2;
+}
+
+void	rrb(t_a *stack_b)
+{
+	int tempo1;
+	int tempo2;
+	int i;
+
+	i = 0;
+	tempo1 = stack_b->tab[0];
+	while (i < stack_b->size)
+	{
+		tempo2 = tempo1;
+		tempo1 = stack_b->tab[i + 1];
+		stack_b->tab[i + 1] = tempo2;
+		i++;
+	} 
+	stack_b->tab[0] = tempo2;
+}
