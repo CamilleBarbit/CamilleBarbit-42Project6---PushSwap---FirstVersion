14e38d1cdf65b7b9ddb2515c12629d21036f62a1
Camille BARBIT <cbarbit@e1r9p17.42.fr>
2022-03-01 17:44:29 +0100

    Modifications done

--- errors/parsing_args.c
+++ errors/parsing_args.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/01 16:43:40 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/01 16:43:42 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/01 17:30:46 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,4 +33,25 @@ int	arg_is_valid(char *str)
 		i++;
 	}
 	return (0);
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	check_overflow(char *str1)
+{
+	int		nb;
+	char	*str2;
+
+	nb = ft_atoi(str1);
+	str2 = ft_itoa(nb);
+	
+
 }
