e89cfcb911317ab947826116d4dd0d3a8b216cc1
Camille BARBIT <cbarbit@e3r4p4.clusters.42paris.fr>
2022-03-01 12:10:15 +0100

    Working with structure

--- Instructions/rotate.c
+++ Instructions/rotate.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 19:24:45 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/01 11:16:20 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/01 12:06:03 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,17 +18,17 @@ void	ra(t_a *stack_a)
 	int tempo2;
 	int i;
 
-	i = stack_a->size_a - 2;
-	tempo1 = stack_a->tab_a[i];
-	stack_a->tab_a[stack_a->size_a - 1] = stack_a->tab_a[0];
+	i = stack_a->size - 2;
+	tempo1 = stack_a->tab[i];
+	stack_a->tab[stack_a->size - 1] = stack_a->tab[0];
 	while (i > 0)
 	{
 		tempo2 = tempo1;
-		tempo1 = stack_a->tab_a[i - 1];
-		stack_a->tab_a[i - 1] = tempo2;
+		tempo1 = stack_a->tab[i - 1];
+		stack_a->tab[i - 1] = tempo2;
 		i--;
 	}
-	stack_a->tab_a[stack_a->size_a -1] = tempo1;
+	stack_a->tab[stack_a->size -1] = tempo1;
 }
 
 // void	test(int *tab, int size)
@@ -49,26 +49,26 @@ void	ra(t_a *stack_a)
 // 	tab[size - 1] = tempo1;
 // }
 
-void	rb(t_b *stack_b)
+void	rb(t_a *stack_b)
 {
 	int tempo1;
 	int tempo2;
 	int i;
 
-	i = stack_b->size_b - 2;
-	tempo1 = stack_b->tab_b[i];
-	stack_b->tab_b[stack_b->size_b - 1] = stack_b->tab_b[0];
+	i = stack_b->size - 2;
+	tempo1 = stack_b->tab[i];
+	stack_b->tab[stack_b->size - 1] = stack_b->tab[0];
 	while (i > 0)
 	{
 		tempo2 = tempo1;
-		tempo1 = stack_b->tab_b[i - 1];
-		stack_b->tab_b[i - 1] = tempo2;
+		tempo1 = stack_b->tab[i - 1];
+		stack_b->tab[i - 1] = tempo2;
 		i--;
 	}
-	stack_b->tab_b[stack_b->size_b -1] = tempo1;
+	stack_b->tab[stack_b->size -1] = tempo1;
 }
 
-void	rr(t_b *stack_b, t_a *stack_a)
+void	rr(t_a *stack_a, t_a *stack_b)
 {
 	ra(stack_a);
 	rb(stack_b);
