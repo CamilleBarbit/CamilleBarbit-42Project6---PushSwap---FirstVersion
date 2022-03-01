dad2211344ed875829b30362c4e927fe158611dc
Camille BARBIT <cbarbit@e1r9p17.42.fr>
2022-03-01 16:44:22 +0100

    Check that my each argument is a valid number

---
+++ errors/parsing_args.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parsing_args.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 16:43:40 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/03/01 16:43:42 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	arg_is_digit(char a)
+{
+	if (!(a >= '0' && a <= '9'))
+		return (1);
+	return (0);
+}
+
+int	arg_is_valid(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '-' || str[0] == '+')
+		i++;
+	while (str[i])
+	{
+		if (arg_is_digit(str[i]) == 1)
+			return (write(2, "Error\n", 6), 1);
+		i++;
+	}
+	return (0);
+}
