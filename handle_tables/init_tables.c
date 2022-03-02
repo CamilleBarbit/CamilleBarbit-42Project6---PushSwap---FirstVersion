50aa8ce251981db8a436101180c0a5a0290d6401
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 18:20:55 +0100

    Modified init table

--- handle_tables/init_tables.c
+++ handle_tables/init_tables.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 10:35:53 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 14:11:40 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/02 18:19:46 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,12 +29,15 @@ void	init_struct(t_a *stack_a, t_a *stack_b, int argc)
 	stack_b->tab = NULL;
 }
 
-void	fill_stack_a(t_a *stack_a, char *str)
+void	fill_stack_a(t_a *stack_a, char **argv)
 {
-	int			nb;
-	static int	i = 0;
-
-	nb = ft_atoi(str);
+	int		nb;
+	int		i;
+	
+	i = 1;
 	while (i < stack_a->size)
+	{
+		nb = ft_atoi(argv[i]);
 		stack_a->tab[i++] = nb;
+	}
 }
