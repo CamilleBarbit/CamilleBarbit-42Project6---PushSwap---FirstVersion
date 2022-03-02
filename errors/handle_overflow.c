dcf0a5de07816c292ddad2b55bc2b6c8e867f834
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 09:56:43 +0100

    Renamed files

---
+++ errors/handle_overflow.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_overflow.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/01 16:43:40 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/03/02 09:55:58 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+int	check_overflow(char *str1)
+{
+	int		nb;
+	char	*str2;
+
+	nb = ft_atoi(str1);
+	str2 = ft_itoa(nb);
+	
+
+}
