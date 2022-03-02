e70fab0b126b2d7f2d6b67ca909e7868444d0a6c
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 14:28:28 +0100

    All clear for now

--- handle_tables/init_tables.c
+++ handle_tables/init_tables.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 10:35:53 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 12:37:50 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/02 14:11:40 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,11 @@ void	init_struct(t_a *stack_a, t_a *stack_b, int argc)
 {
 	stack_a->size = argc - 1;
 	stack_b->size = argc - 1;
-	stack_a->tab = malloc(sizeof(int) * size);
+	stack_a->tab = malloc(sizeof(int) * stack_a->size);
 	if (!stack_a->tab)
 		return ;
 	stack_a->tab = NULL;
-	stack_b->tab = malloc(sizeof(int) * size);
+	stack_b->tab = malloc(sizeof(int) * stack_b->size);
 	if (!stack_b->tab)
 	{
 		free(stack_a->tab);
