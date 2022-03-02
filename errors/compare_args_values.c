345463bdc79ed2ff91595c7d9d50619c99309093
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 15:06:25 +0100

    Updated

--- errors/compare_args_values.c
+++ errors/compare_args_values.c
@@ -1,35 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handle_overflow.c                                  :+:      :+:    :+:   */
+/*   compare_args_values.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/01 16:43:40 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 14:50:36 by cbarbit          ###   ########.fr       */
+/*   Created: 2022/03/02 14:41:12 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/03/02 15:05:48 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../push_swap.h"
-
-int	ft_strcmp(char *s1, char *s2)
+int compare_each_value(char **str)
 {
-	int	i;
+	int i;
+	int j;
 
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
+	i = 1;
+	j = 2;
+	while (str[j])
+	{
+		while (str[i])
+		{
+			if (ft_strcmp(str[i], str[j]) == 0)
+				return (write(2, "Error\n", 6) 1);
+			j++;
+		}
 		i++;
-	return (s1[i] - s2[i]);
-}
-
-int	check_overflow(char *str1)
-{
-	int		nb;
-	char	*str2;
-
-	nb = ft_atoi(str1);
-	str2 = ft_itoa(nb);
-	if (ft_strcmp(str1, str2) != 0)
-		return (write(2, "Error\n", 6), 1);
+	}
 	return (0);
 }
