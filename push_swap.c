260180a8a555d79316a98c9f5b9e923104dd436f
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 11:24:09 +0100

    Updated version

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,20 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/01 17:23:32 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/02 11:21:37 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	check_all_args(char *str)
+{
+	if (arg_is_valid(str[i]) == 1)
+		return (write(2, "Error\n", 6), 1);
+	if (check_overflow(str[i]) == 1)
+		return (write(2, "Error\n", 6), 1);
+	return (0);
+}
 
 int main(int argc, char **argv)
 {
@@ -21,19 +30,21 @@ int main(int argc, char **argv)
 	i = 1;
 	if (argc > 1)
 	{
+		while (i < argc)
+			if (check_all_args(argv[i++]) == 1)
+				return (1);
 		stack_a = malloc(sizeof(t_a));
 		if (!stack_a)
-			return(1);
+			return (1);
 		stack_b = malloc(sizeof(t_a));
 		if (!stack_b)
-			return(1);
+			return(free(stack_a), 1);
+		init_stacks(stack_a, stack_b, argc);
+		i = 1;
 		while (i < argc)
-		{
-			if (arg_is_valid(argv[i]) == 1)
-				return (write(2, "Error\n", 6), 1);
-			
-			i++;
-		}
+			fill_stack_a(stack_a, argv[i++] argc);
+		// free(stack_a);
+		// free(stack_b);
 	}
 	return (0);
 }
