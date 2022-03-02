f3a06d5c48621a88b30bd057a9f281ac52936377
Camille Barbit <barbitcamille@gmail.com>
2022-03-02 20:32:01 +0100

    Added a function to check if stack_a is in order

---
+++ handle_tables/check_stack_a.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_stack_a.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 20:16:57 by camillebarb       #+#    #+#             */
+/*   Updated: 2022/03/02 20:31:24 by camillebarb      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+//I check if it is in order only if argc > 3
+int	check_if_in_order(t_a *stack_a)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = i + 1;
+	while (j < stack_a->size)
+	{
+		if (tab[i] > tab[j])
+			return(1);
+		i++;
+		j++;
+		
+	}
+}
