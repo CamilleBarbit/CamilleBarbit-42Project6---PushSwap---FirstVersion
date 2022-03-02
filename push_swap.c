7aa2c2620dacd1b2f6b14a406095625c0b0c035d
Camille BARBIT <cbarbit@e2r4p5.clusters.42paris.fr>
2022-03-02 15:34:49 +0100

    Updated

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 15:13:02 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/02 15:31:45 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	check_all_args(char **argv, int argc)
 	while (i < argc)
 	{
 		if (arg_is_valid(argv[i]) == 1)
-			return (write(2, "Error\n", 6), 1);
+			return (1);
 		if (check_overflow(argv[i]) == 1)
-			return (write(2, "Error\n", 6), 1);
+			return (1);
 	}
-	if (compare_each_value(argv) == 0)
+	if (compare_each_value(argv, argc) == 0)
 		return (1);
 	return (0);
 }
@@ -39,7 +39,7 @@ int main(int argc, char **argv)
 	if (argc > 1)
 	{
 		if (check_all_args(argv, argc) == 1)
-			return (1);
+			return (write(2, "Error\n", 6), 1);
 		stack_a = malloc(sizeof(t_a));
 		if (!stack_a)
 			return (1);
