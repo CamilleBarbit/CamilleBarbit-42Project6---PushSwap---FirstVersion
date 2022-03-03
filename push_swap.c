fc2dd647378e036145bffe7055372075c868c993
Camille Barbit <barbitcamille@gmail.com>
2022-03-03 08:21:30 +0100

    Added a i++ to the function check_all_args

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 20:39:34 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/03 08:18:56 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@ int	check_all_args(char **argv, int argc)
 			return (1);
 		if (compare_each_value(argv, i) == 1)
 			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -36,8 +37,10 @@ int main(int argc, char **argv)
 
 	if (argc > 1)
 	{
+		printf("Je vais ici");
 		if (check_all_args(argv, argc) == 1)
 			return (write(2, "Error\n", 6), 1);
+		printf("Je vais là");
 		stack_a = malloc(sizeof(t_a));
 		if (!stack_a)
 			return (1);
@@ -46,8 +49,8 @@ int main(int argc, char **argv)
 			return (free(stack_a), 1);
 		init_struct(stack_a, stack_b, argc);
 		fill_stack_a(stack_a, argv);
-		if (check_if_in_order(stack_a) == 1)
-			return (1);
+		// if (check_if_in_order(stack_a) == 1)
+		// 	return (write(2, "Error\n", 6), 1);
 		// free(stack);
 	}
 	return (0);
