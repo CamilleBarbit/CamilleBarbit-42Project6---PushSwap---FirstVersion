55b8df1835c7800011c78ce03ab52369d8b31278
Camille Barbit <barbitcamille@gmail.com>
2022-03-03 08:56:07 +0100

    Added explicit command

--- instructions/swap.c
+++ instructions/swap.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   swap.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:20:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/01 14:35:23 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/03 08:45:50 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,18 @@ void	swap(t_a *stack)
 void	sa(t_a *stack_a)
 {
 	swap(stack_a);
+	ft_printf("sa\n");
 }
 
 void	sb(t_a *stack_b)
 {
 	swap(stack_b);
+	ft_printf("sb\n");
 }
 
 void	ss(t_a *stack_a, t_a *stack_b)
 {
 	sa(stack_a);
 	sb(stack_b);
+	ft_printf("ss\n");
 }
