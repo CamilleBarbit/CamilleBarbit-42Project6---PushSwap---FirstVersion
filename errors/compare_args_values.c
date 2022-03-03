3eb8db54204e1ebc6bb843e380f1fbb51818b6c6
Camille BARBIT <cbarbit@e1r11p15.42.fr>
2022-03-03 17:38:48 +0100

    Nothing changed much

--- errors/compare_args_values.c
+++ errors/compare_args_values.c
@@ -6,20 +6,20 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 14:41:12 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 18:48:11 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/03 17:12:36 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-int	compare_each_value(char **argv, int i)
+int	compare_each_value(char **argv, int i, int argc)
 {
 	int	j;
-	
+
 	j = i + 1;
 	while (argv[i])
-	{	
-		while (argv[j])
+	{
+		while (j < argc)
 		{
 			if (ft_strcmp(argv[i], argv[j++]) == 0)
  				return (1);
