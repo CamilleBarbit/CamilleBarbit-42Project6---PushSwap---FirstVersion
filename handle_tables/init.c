da47a1854283b4b50fe0cd4657a79dfeec3bba96
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 15:01:04 +0100

    INITIALISING STRUCT

---
+++ handle_tables/init.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/02 10:35:53 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/03/04 14:59:16 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	init_struct(t_a *stack_a, t_a *stack_b, int size)
+{
+	stack_a->size = size;
+	stack_b->size = size;
+	stack_a->tab = malloc(sizeof(int) * stack_a->size);
+	if (!stack_a->tab)
+		return ;
+	stack_a->tab = NULL;
+	stack_b->tab = malloc(sizeof(int) * stack_b->size);
+	if (!stack_b->tab)
+	{
+		free(stack_a->tab);
+		return ;
+	}
+	stack_b->tab = NULL;
+}
+
+void	fill_stack_a(t_a *stack_a, char **str, int i)
+{
+	int		nb;
+	int		i;
+	int		j;
+
+	j = 0;
+	while (j < stack_a->size)
+	{
+		nb = ft_atoi(str[i]);
+		stack_a->tab[j++] = nb;
+	}
+}
