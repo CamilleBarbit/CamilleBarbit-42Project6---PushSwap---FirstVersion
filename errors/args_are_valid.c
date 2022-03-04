7fb43648cdc3b08737b3a9400c4f76dd7c544b33
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 11:50:25 +0100

    All args errors should be handled

--- errors/args_are_valid.c
+++ errors/args_are_valid.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 09:55:16 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/04 11:32:47 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 11:46:23 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,24 @@ static int	arg_is_digit(char *str, int i)
 	return (1);
 }
 
+static int	arg_is_only_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		if (str[i++] != ' ')
+			return (0);
+	return (1);
+}
+
 int	arg_is_valid(char *str)
 {
 	int	i;
 
 	i = 0;
+	if (arg_is_only_space(str) == 1)
+		return (1);
 	if (str[0] == '-')
 		i++;
 	while (str[i])
