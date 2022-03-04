04888a02c50afd899737cfed37f7b7600f884821
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 15:00:00 +0100

    INITIALISING STRUCT

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/04 14:33:49 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 14:57:10 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,21 @@ int main(int argc, char **argv)
 	t_a	*stack_a = NULL;
 	t_a *stack_b = NULL;
 
-	if (argc == 2)
+	if (argc == 2) //PARSING OK
 	{
+		stack_a = malloc(sizeof(t_a));
+		if (!stack_a)
+			return (1);
+		stack_b = malloc(sizeof(t_a));
+		if (!stack_b)
+			return (free(stack_a), 1);
 		if (argv1_is_valid(argv) == 1)
-			return (printf("ERROR 2 ARGS\n"), 1);
-		if (check_first_arg(argv[1]) == 1)
-			return (printf("ERROR OVERFLOW OR DOUBLE\n"), 1);
+			return (free(stack_a), free(stack_b), printf("ERROR 2 ARGS\n"), 1);
+		if (check_first_arg(argv[1], stack_a, stack_b) == 1)
+			return (free(stack_a), free(stack_b), printf("ERROR OVERFLOW OR DOUBLE\n"), 1);
+		
 	}
-	if (argc > 2)
+	if (argc > 2) //PARSING OK
 	{
 		if (check_all_args(argv, argc) == 1)
 			return (printf("ERROR MORE THANTWO ARGS"), 1);
@@ -52,8 +59,8 @@ int main(int argc, char **argv)
 		stack_b = malloc(sizeof(t_a));
 		if (!stack_b)
 			return (free(stack_a), 1);
-		init_struct(stack_a, stack_b, argc);
-		fill_stack_a(stack_a, argv);
+		init_struct(stack_a, stack_b, argc - 1);
+		fill_stack_a(stack_a, argv, 1);
 		// if (check_if_in_order(stack_a) == 1)
 		// 	return (write(2, "Error\n", 6), 1);
 		// free(stack);
