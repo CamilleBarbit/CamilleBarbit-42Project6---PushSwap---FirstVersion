3e00f55bb2acd0a567f2329a2fd64edf15a1ebf3
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 16:51:30 +0100

    Only checking if it is in order if ther are more than one number in my argv1

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/04 15:26:29 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 16:48:53 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,10 @@ int	check_all_args(char **argv, int argc)
 	return (0);
 }
 
+/*
+FONCTION DE TEST POUR AFFICHER LE TABLEAU AVANT TRI
+*/
+
 static void	show_tab(t_a *stack_a)
 {
 	int i;
@@ -39,6 +43,9 @@ static void	show_tab(t_a *stack_a)
 		printf("%d\n", stack_a->tab[i++]);
 }
 
+/*
+MAIN PROGRAM
+*/
 
 int main(int argc, char **argv)
 {
@@ -54,15 +61,17 @@ int main(int argc, char **argv)
 		if (!stack_b)
 			return (free(stack_a), 1);
 		if (argv1_is_valid(argv) == 1)
-			return (free(stack_a), free(stack_b), printf("ERROR 2 ARGS\n"), 1);
+			return (free(stack_a), free(stack_b), printf("ERROR\nInvalid arguments\n"), 1);
 		if (check_first_arg(argv[1], stack_a, stack_b) == 1)
-			return (free(stack_a), free(stack_b), printf("ERROR OVERFLOW OR DOUBLE\n"), 1);
-		
+			return (free(stack_a), free(stack_b), printf("ERROR\nOverflowOrDouble"), 1);
+		show_tab(stack_a);
+		if (stack_a->size > 1)
+			sort_three(stack_a);
 	}
 	if (argc > 2) //PARSING OK
 	{
 		if (check_all_args(argv, argc) == 1)
-			return (printf("ERROR MORE THANTWO ARGS"), 1);
+			return (printf("ERROR\nInvalid arguments\n"), 1);
 		stack_a = malloc(sizeof(t_a));
 		if (!stack_a)
 			return (1);
@@ -72,6 +81,7 @@ int main(int argc, char **argv)
 		init_struct(stack_a, stack_b, argc - 1);
 		fill_stack_a(stack_a, argv, 1);
 		show_tab(stack_a);
+		sort_three(stack_a);
 		// if (check_if_in_order(stack_a) == 1)
 		// 	return (write(2, "Error\n", 6), 1);
 		// free(stack);
