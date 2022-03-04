310cdb848e4f57f5a3dd25ac9131e8eee4085764
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 11:33:31 +0100

    Improved functions to make ure that it works if I have a string as argument

--- errors/args_are_valid.c
+++ errors/args_are_valid.c
@@ -6,17 +6,21 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 09:55:16 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/02 15:26:43 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 11:32:47 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
 
-static int	arg_is_digit(char a)
+static int	arg_is_digit(char *str, int i)
 {
-	if (!(a >= '0' && a <= '9'))
-		return (1);
-	return (0);
+	if (str[i] >= '0' && str[i] <= '9')
+		return (0);
+	else if (str[i] == ' ')
+		return (0);
+	else if (str[i] == '-' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
+		return (0);
+	return (1);
 }
 
 int	arg_is_valid(char *str)
@@ -28,7 +32,7 @@ int	arg_is_valid(char *str)
 		i++;
 	while (str[i])
 	{
-		if (arg_is_digit(str[i]) == 1)
+		if (arg_is_digit(str, i) == 1)
 			return (1);
 		i++;
 	}
