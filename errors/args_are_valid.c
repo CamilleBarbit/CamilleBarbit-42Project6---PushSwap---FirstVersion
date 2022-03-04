9201e4f6a0a6295173565b2991d4054e8d2d7166
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 12:23:56 +0100

    UPDATED VERSION

--- errors/args_are_valid.c
+++ errors/args_are_valid.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 09:55:16 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/04 11:46:23 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 12:23:26 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,9 @@
 
 static int	arg_is_digit(char *str, int i)
 {
-	if (str[i] >= '0' && str[i] <= '9')
-		return (0);
-	else if (str[i] == ' ')
-		return (0);
-	else if (str[i] == '-' && (str[i + 1] >= '0' && str[i + 1] <= '9'))
-		return (0);
-	return (1);
-}
-
-static int	arg_is_only_space(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		if (str[i++] != ' ')
-			return (0);
-	return (1);
+	if (!(str[i] >= '0' && str[i] <= '9'))
+		return (1);
+	return (0);
 }
 
 int	arg_is_valid(char *str)
@@ -39,8 +24,6 @@ int	arg_is_valid(char *str)
 	int	i;
 
 	i = 0;
-	if (arg_is_only_space(str) == 1)
-		return (1);
 	if (str[0] == '-')
 		i++;
 	while (str[i])
