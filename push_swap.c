c3d90cc3f81d1870b391dccbc61f03de91ab2b1b
Camille BARBIT <cbarbit@e2r4p4.clusters.42paris.fr>
2022-03-04 14:34:33 +0100

    Parsing OK

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/04 12:40:21 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/04 14:33:49 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@ int	check_all_args(char **argv, int argc)
 	while (i < argc)
 	{
 		if (arg_is_valid(argv[i]) == 1)
-			return (printf("PASVALIDE"), 1);
+			return (1);
 		if (check_overflow(argv[i]) == 1)
-			return (printf("OVERFLOW"), 1);
+			return (1);
 		if (compare_each_value(argv, i, argc) == 1)
-			return (printf("DOUBLON"), 1);
+			return (1);
 		i++;
 	}
 	return (0);
@@ -37,13 +37,15 @@ int main(int argc, char **argv)
 
 	if (argc == 2)
 	{
-		if argv1_is valid //pour checker que argv1 est valide
-		check_first_arg(argv[1]);
+		if (argv1_is_valid(argv) == 1)
+			return (printf("ERROR 2 ARGS\n"), 1);
+		if (check_first_arg(argv[1]) == 1)
+			return (printf("ERROR OVERFLOW OR DOUBLE\n"), 1);
 	}
 	if (argc > 2)
 	{
 		if (check_all_args(argv, argc) == 1)
-			return (printf("ERROR CA MERE"), 1);
+			return (printf("ERROR MORE THANTWO ARGS"), 1);
 		stack_a = malloc(sizeof(t_a));
 		if (!stack_a)
 			return (1);
