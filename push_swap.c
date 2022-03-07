3be37dff698f1355b01b8521fe7d5f59b6b4eeef
Camille BARBIT <cbarbit@e1r12p14.42.fr>
2022-03-07 16:24:32 +0100

    Updated version - main algorithm with th lis

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 16:21:02 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/07 10:39:28 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/07 16:18:27 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -86,7 +86,8 @@ int main(int argc, char **argv)
 			return (free(stack_a), 1);
 		init_struct(stack_a, stack_b, argc - 1);
 		fill_stack_a(stack_a, argv, 1);
-		sort_five(stack_a, stack_b);
+		whatever(stack_a);
+		//sort_five(stack_a, stack_b);
 		//if (check_if_in_order(stack_a) == 0)
 				// sort_three(stack_a);
 		// else
