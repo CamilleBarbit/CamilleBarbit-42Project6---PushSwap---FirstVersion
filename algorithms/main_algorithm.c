5319aac33e5ea59df7239f36c63969ec601ea839
Camille Barbit <barbitcamille@gmail.com>
2022-03-10 11:04:42 +0100

    Cleaned the code

--- algorithms/main_algorithm.c
+++ algorithms/main_algorithm.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_algorithm.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:03:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/09 16:39:10 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/10 11:00:44 by camillebarb      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,16 @@
 /*
 FONCTION DE TEST POUR AFFICHER LE TABLEAU TRIE
 */
-static void	print_tab(int *tab, int size)
-{
-	int	i;
+// static void	print_tab(int *tab, int size)
+// {
+// 	int	i;
 
-	i = 0;
-	while (i < size)
-	{
-		printf("%d\n", tab[i++]);
-	}
-}
+// 	i = 0;
+// 	while (i < size)
+// 	{
+// 		printf("%d\n", tab[i++]);
+// 	}
+// }
 
 /*
 STEP 1: Function to place the minimum on top of stack_a->tab
@@ -49,31 +49,14 @@ void	place_it_on_top(t_a *stack_a, int min_pos)
 	}
 }
 
-void	whatever(t_a *stack_a)
+void	get_lis(t_a *stack_a)
 {
 	int	j;
-	int	i;
-	int	k;
-	int	lis_max;
-	int	lis_max_pos;
 
-	i = 0;
-	k = 0;
-	j = look_for_smallest_num(stack_a); //je cherche le minimum dans la stack_a
+	j = look_for_smallest_num(stack_a); //je cherche la position du minimum dans la stack_a
 	if (j != 0) //si le minimum est ailleurs qu'a la premiere position, alors je le mets on top
 		place_it_on_top(stack_a, j);
-	printf("TAB A:\n");
-	print_tab(stack_a->tab, stack_a->size);
-	// printf("\n\n\n");
-	// printf("TAB TEMP:\n");
-	// print_tab(tmp, stack_a->size);
-	test_lis(stack_a); //Je recupere mon tableau avec toutes les lis possibles
-	lis_max = find_lis_max(stack_a); //Je determine la longueur de ma LIS
-	lis_max_pos = get_lis_max_pos(stack_a, lis_max); //J'ai le position de mon LIS_MAX
-	printf("LIS MAX: %d\n", lis_max);
-	printf("LIS MAX POSITION: %d\n", lis_max_pos);
-	get_sub_sequence(stack_a, lis_max, lis_max_pos);
-	//printf("LIS TAB:\n");
-	//print_tab(stack_a->lis_tab, stack_a->size);
-
+	test_lis(stack_a); //Je recupere mon tableau avec toutes les listes de int ordonnés possibles
+	find_lis_max(stack_a); //Je determine la longueur de ma LIS
+	get_sub_sequence(stack_a);
 }
