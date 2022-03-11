b2fd2609db3a075b2ad74bc45add087a82150bc5
Camille BARBIT <cbarbit@e2r4p21.clusters.42paris.fr>
2022-03-11 17:24:41 +0100

    Added functions to count moves

--- algorithms/count_moves.c
+++ algorithms/count_moves.c
@@ -6,11 +6,36 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 13:44:45 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/11 15:15:08 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/11 17:23:43 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../push_swap.h"
+
+/*
+Function to look for the biggest num in the stack
+*/
+int	look_for_biggest_num(t_a *stack_a)
+{
+	int	i;
+	int	j;
+	int	tmp;
+
+	i = 0;
+	tmp = stack_a->tab[0];
+	j = 0;
+	while (i < stack_a->size)
+	{
+		if (stack_a->tab[i] > tmp)
+		{
+			tmp = stack_a->tab[i];
+			j = i;
+		}
+		i++;
+	}
+	//printf("THE INDEX OF BIGGEST NUM in A: %d\n", j);
+	return (j);
+}
 
 /*
 Function to check if my number is the maximum in stack_a or/and stack_b
@@ -56,20 +81,30 @@ int	count_moves_in_b(t_a *stack_b, int pos)
 	return (count);
 }
 
-
-int	count_move_in_a(t_a *stack_b, t_a *stack_a, int pos)
+/*
+Function to find the position of the number directly inferior to my number -> It must be the closest number > to the one I have in stack_b
+*/
+int	find_closest_nb(t_a *stack_a, int nb)
 {
 	int	i;
 	int	j;
-	int	count;
-
+	int	tempo;
+	
 	i = 0;
-	j = stack_b->size - 1;
-	count = 1; //J'intialise count a 1 car dans tous les cas il faut faire pa pour push mon nombre de stack_b a stack_a
-	if (stack_a->tab[j] > stack_b->tab[pos] && stack_b->tab[pos] < stack_a->tab[i])
-		count++;
-	while (i < stack_a->size / 2)
+	j = look_for_biggest_num(stack_a);
+	tempo = stack_a->tab[j] - nb;
+	j = 0;
+	while (i < stack_a->size)
 	{
-		
+		if (stack_a->tab[i] > nb)
+		{
+			if (stack_a->tab[i] - nb < tempo)
+			{
+				tempo = stack_a->tab[i] - nb;
+				j = i;
+			}
+		}
+		i++;
 	}
+	return (j);
 }
