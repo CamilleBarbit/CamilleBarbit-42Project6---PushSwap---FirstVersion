398cea35f62a1ce00c41d9765b7ed50bdd735aff
Camille BARBIT <cbarbit@e1r10p15.42.fr>
2022-03-11 14:21:19 +0100

    Added new file

--- push_swap.h
+++ push_swap.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   push_swap.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: camillebarbit <camillebarbit@student.42    +#+  +:+       +#+        */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/02 15:16:40 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/10 14:47:26 by camillebarb      ###   ########.fr       */
+/*   Updated: 2022/03/11 14:07:22 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,5 +73,6 @@ int		check_first_arg(char *str, t_a *stack_a, t_a *stack_b);
 int		look_for_smallest_num(t_a *stack_a);
 int		get_lis_max_pos(t_a *stack_a, int lis_max);
 int		check_if_in_sub_sequence(t_a *stack_a, int i);
+int		count_moves_in_b(t_a *stack_b, int pos);
 
 #endif
