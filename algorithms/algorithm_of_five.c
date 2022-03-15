812879bcdbfee777c99d203021363d524f9759ca
Camille BARBIT <cbarbit@e2r4p6.clusters.42paris.fr>
2022-03-15 10:50:40 +0100

    Made it cleaner

--- algorithms/algorithm_of_five.c
+++ algorithms/algorithm_of_five.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 17:10:37 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/11 14:50:36 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/15 10:25:17 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,30 +36,6 @@ void	push_in_b(t_a *stack_a, t_a *stack_b, int pos)
 		pb(stack_a, stack_b);
 	}
 
-}
-
-/*
-Function to look for the smallest number in stack_a, in order to put it on top
-*/
-int	look_for_smallest_num(t_a *stack_a)
-{
-	int	i;
-	int	j;
-	int	tmp;
-
-	i = 0;
-	tmp = stack_a->tab[0];
-	j = 0;
-	while (i < stack_a->size)
-	{
-		if (tmp > stack_a->tab[i])
-		{
-			tmp = stack_a->tab[i];
-			j = i;
-		}
-		i++;
-	}
-	return (j);
 }
 
 void	sort_five(t_a *stack_a, t_a *stack_b)
