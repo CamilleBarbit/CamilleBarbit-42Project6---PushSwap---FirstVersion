145f67627f2e363cb4baff543dc77323ff83dfc5
Camille BARBIT <cbarbit@e1r10p23.42.fr>
2022-03-17 12:35:06 +0100

    Added new file

---
+++ algorithms/push_back_in_a.c
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_back_in_a.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/17 12:27:50 by cbarbit           #+#    #+#             */
+/*   Updated: 2022/03/17 12:30:52 by cbarbit          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+void	check_move_in_a(t_a *stack_a)
+{
+	if (stack_a->moves[0] < 0)
+	{
+		while(stack_a->moves[0] != 0)
+		{
+			ra(stack_a);
+			stack_a->moves[0]++;
+		}
+	}
+	if (stack_a->moves[0] > 0)
+	{
+		while(stack_a->moves[0] != 0)
+		{
+			rra(stack_a);
+			stack_a->moves[0]--;
+		}
+	}
+}
+
+void	check_move_in_b(t_a *stack_a, t_a *stack_b)
+{
+	if (stack_a->moves[1] < 0)
+	{
+		while(stack_a->moves[1] != 0)
+		{
+			rb(stack_b);
+			stack_a->moves[1]++;
+		}
+	}
+	if (stack_a->moves[1] > 0)
+	{
+		while(stack_a->moves[1] != 0)
+		{
+			rrb(stack_b);
+			stack_a->moves[1]--;
+		}
+	}
+}
+
+void	check_move_in_a_and_b(t_a *stack_a, t_a *stack_b)
+{
+	if (stack_a->moves[2] < 0)
+	{
+		while(stack_a->moves[2] != 0)
+		{
+			rr(stack_a, stack_b);
+			stack_a->moves[2]++;
+		}
+	}
+	if (stack_a->moves[2] > 0)
+	{
+		while(stack_a->moves[2] != 0)
+		{
+			rrr(stack_a, stack_b);
+			stack_a->moves[2]--;
+		}
+	}
+}
+
