1c30b358d7c809f2d787a4f11d607ca586a265c8
Camille BARBIT <cbarbit@e2r12p9.clusters.42paris.fr>
2022-03-21 15:31:27 +0100

    changed a condition

--- algorithms/main_algorithm.c
+++ algorithms/main_algorithm.c
@@ -6,7 +6,7 @@
 /*   By: cbarbit <cbarbit@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 11:03:20 by cbarbit           #+#    #+#             */
-/*   Updated: 2022/03/20 16:33:47 by cbarbit          ###   ########.fr       */
+/*   Updated: 2022/03/21 15:27:38 by cbarbit          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,16 +27,19 @@ void	turn_moves_into_action(t_a *stack_a, t_a *stack_b)
 {
 	int	i;
 	int	j;
+	int	k;
 
 	i = stack_b->size;
 	while (i > 0)
 	{
+		k = find_closest_nb(stack_a, stack_b->tab[i]);
 		compare_total_moves_count(stack_a, stack_b);
 		check_move_in_a(stack_a);
 		check_move_in_b(stack_a, stack_b);
 		check_move_in_a_and_b(stack_a, stack_b);
 		pa(stack_a, stack_b);
-		if (is_nb_max_in_stack(stack_a, stack_a->tab[0]) == 1)
+		// if (is_nb_max_in_stack(stack_a, stack_a->tab[0]) == 1)
+		if (stack_b->tab[k] < stack_a->tab[i])
 			sa(stack_a);
 		i--;
 	}
